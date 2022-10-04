df35d989b19f04080e63d8dbb7c0d701c6e48c42
yakdd <yakdd@yandex.ru>
2022-10-04 11:13:38 +0300

    Homework tasks 02,04,06,08

---
+++ Task02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2; b = 10 -> max = 10
+// a = -9; b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"max = {num1}");
+}
+else
+{
+    Console.WriteLine($"max = {num2}");
+}
---
+++ Task08/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0)
+{
+    Console.WriteLine("Вы ввели отрицательное число!");
+}
+else
+{
+    for (int i = 1; i <= num; i++)
+    {
+        if ((i == num || i == num - 1) && i % 2 == 0)
+        {
+            Console.Write(i);
+        }
+        else
+        {
+            if (i % 2 == 0) Console.Write($"{i}, ");
+        }
+    }
+
+}
