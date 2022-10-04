a8ad40f1a081ae3cfd64e0bc66ebdaeef9cb4b95
yakdd <yakdd@yandex.ru>
2022-10-04 11:13:38 +0300

    Homework tasks 02,04,06,08

---
+++ Task04/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+
+Console.Write("Введите первое число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num2 > max) max = num2;
+
+Console.Write("Введите трерье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num3 > max) max = num3;
+
+Console.Write(max);
---
+++ Task08/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    if ((i == num || i == num - 1) && i % 2 == 0)
+    {
+        Console.Write(i);
+    }
+    else
+    {
+        if (i % 2 == 0) Console.Write($"{i}, ");
+    }
+}
