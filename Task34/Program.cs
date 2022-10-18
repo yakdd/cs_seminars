cdcb42074666711547d92cb536d91ee34a57a29a
yakdd <yakdd@yandex.ru>
2022-10-18 13:16:18 +0300

    Homework 5, task 34,36,38

---
+++ Task34/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArray(int num)
+{
+    int[] array = new int[num];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+    return array;
+}
+
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.WriteLine($"{array[i]}]");
+    }
+}
+
+
+
+int CountEvenElements(int[] array)
+{
+    int count = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+
+
+Console.Write("Введите количество элементов массива: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+
+int[] myArr = CreateArray(numbers);
+PrintArray(myArr);
+
+int countEven = CountEvenElements(myArr);
+Console.WriteLine($"Количество чётных чисел в массиве -> {countEven}");
---
+++ Task38/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+double[] CreateArrayRndDouble(int num, int min, int max)
+{
+    double[] array = new double[num];
+    var rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        double rndNum = rnd.NextDouble() * (max - min) + min;
+        array[i] = Math.Round(rndNum, 2);
+    }
+    return array;
+}
+
+
+
+double FindMax(double[] array)
+{
+    double maxElem = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > maxElem) maxElem = array[i];
+    }
+    return maxElem;
+}
+
+
+
+double FindMin(double[] array)
+{
+    double minElem = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < minElem) minElem = array[i];
+    }
+    return minElem;
+}
+
+
+
+void PrintArrayDbl(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.WriteLine($"{array[i]}]");
+    }
+}
+
+
+
+double[] myArr = CreateArrayRndDouble(10, 0, 99);
+PrintArrayDbl(myArr);
+
+double findMax = FindMax(myArr);
+double findMin = FindMin(myArr);
+
+Console.WriteLine($"Разница между максимальным ({findMax}) и минимальным ({findMin}) элементами массива = {findMax - findMin}");
