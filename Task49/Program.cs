152d9c2934778e2b95bb6f0fecc0f54e2594844f
yakdd <yakdd@yandex.ru>
2022-10-21 23:44:52 +0300

    seminar 7 finished

---
+++ Task49/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    var matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+
+        }
+    }
+    return matrix;
+}
+
+
+void ChangeMatrix(int[,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j += 2)
+        {
+            array2D[i, j] *= array2D[i, j];
+        }
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2D);
+
+Console.WriteLine();
+
+ChangeMatrix(array2D);
+PrintMatrix(array2D);
---
+++ Task51/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    var matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+// int SumElements(int[,] matrix)
+// {
+//     int sum = default;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (i == j) sum += matrix[i, j];
+//         }
+//     }
+//     return sum;
+// }
+
+
+int SumElements2(int[,] matrix)
+{
+    int sum = default;
+    int demention = (matrix.GetLength(0) > matrix.GetLength(1)) ? 1 : 0;
+    for (int i = 0; i < matrix.GetLength(demention); i++) sum += matrix[i, i];
+    return sum;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($" {matrix[i, j],2} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+int[,] array2D = CreateMatrixRndInt(4, 3, 1, 10);
+PrintMatrix(array2D);
+
+int sumElements = SumElements2(array2D);
+Console.WriteLine(sumElements);
