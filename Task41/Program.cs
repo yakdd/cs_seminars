c7916d2d90806aee7f9edaa5c8c88d1c4c510548
yakdd <yakdd@yandex.ru>
2022-10-21 16:40:22 +0300

    Homework 6, task 41,43

---
+++ Task41/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+// Вариант без сохранения введенных чисел:
+int PositiveNumbers1(int num)
+{
+    int count = default;
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number > 0) count++;
+    }
+    return count;
+}
+Console.WriteLine();
+
+
+// Вариант с сохранением введенных чисел:
+int PositiveNumbers2(int num)
+{
+    int[] array = new int[num];
+    int count = default;
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        if (array[i] > 0) count++;
+    }
+    PrintArray(array);
+    return count;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+
+
+Console.Write("Сколько чисел будем вводить? ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int positiveNumbers1 = PositiveNumbers1(m);
+Console.WriteLine($"Количество введенных положительных чисел -> {positiveNumbers1}.");
+
+int positiveNumbers2 = PositiveNumbers2(m);
+Console.WriteLine($"Количество введенных положительных чисел -> {positiveNumbers2}.");
---
+++ Task43/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+double[] SetOfEquations(double a, double b, double c, double d)
+{
+    double[] coords = new double[2];
+    coords[0] = Math.Round(((d - b) / (a - c)), 3);
+    coords[1] = Math.Round((a * coords[0] + b), 3);
+    return coords;
+}
+
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+
+Console.Write("Введите первый коэффициент k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите первую константу b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второй коэффициент k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второую константу b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double[] result = SetOfEquations(a: k1, b: b1, c: k2, d: b2);
+Console.Write($"Координаты точки пересечения прямых -> ");
+PrintArray(result);
