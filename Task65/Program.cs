c9e96bbf2d362b51ecc077ad55d746820d79c1fa
yakdd <yakdd@yandex.ru>
2022-11-12 22:48:42 +0300

    seminar 9 finished

---
+++ Task65/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+
+Console.Write("Введите первое целое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers(int num1, int num2)
+{
+    if (num1 == num2) Console.Write($"{num1} ");
+    if (num1 < num2)
+    {
+        Console.Write($"{num1} ");
+        NaturalNumbers(num1 + 1, num2);
+    }
+    if (num1 > num2)
+    {
+        Console.Write($"{num1} ");
+        NaturalNumbers(num1 - 1, num2);
+    }
+}
+
+
+NaturalNumbers(m, n);
---
+++ Task67/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int SummDigits(int num)
+{
+    if (num == 0) return 0;
+    return num % 10 + SummDigits(num / 10);
+}
+
+
+int sum = SummDigits(453);
+Console.WriteLine(sum);
